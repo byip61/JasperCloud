1bd053af9792d9e3774f9fee1dff61494ccac69f
mCreese-mancol <mcreese01@manhattan.edu>
2024-11-30 21:24:01 -0500

    Implement rudimentary file search Populates the fileNames hash set with names matching the query.

--- JasperCloud/Controllers/FileSearchController.cs
+++ JasperCloud/Controllers/FileSearchController.cs
@@ -7,18 +7,71 @@ namespace JasperCloud.Controllers
     public class FileSearchController : Controller
     {
         public JasperCloud.Models.File[]? files;
-        public List<string>? fileNames;
+        public HashSet<string> fileNames = [];
 
-        public void FindFiles(string query, string directory)
+        //
+        // Not intended to be used anywhere else; simply splits up the string given a few options.
+        //
+        private static string[] SplitMe(string input, bool symbols = false)
+        {
+            char[]? delimiters;
+
+            if (symbols)
+            {
+                // Only account for spaces, and other "separator" symbols.
+                delimiters = [' ', ',', '.', '_'];
+            }
+            else
+            {
+                // Include even "impossible" ASCII characters.
+                delimiters = [ '!', '@', '#', '$',
+                                      '%', '^', '&', '*',
+                                      '(', ')', '[', ']',
+                                      '{', '}', ':', ';',
+                                      '\'', '\"', '\\', '/',
+                                      '<', '>', '~', '`',
+                                      '+', '-', '_', ' ',
+                                      '.', ',' ];
+            }
+
+            string[] substrings = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            return substrings;
+        }
+
+        public void FindFiles(string query, string directory, bool symbols = false)
         {
             /* TODO: 
-             * Split query so individual words are searched on their own. (partial matches)
-             * Concatenate string list to account for this split query.
              * Connect to database and gather file IDs, make sure found files are linked to the user via user_id.
              * Relay search results to the files variable, so that they can be displayed on the frontend.
              */
             // https://stackoverflow.com/questions/51135520/search-for-file-in-c-sharp
-            fileNames = (List<string>?)Directory.EnumerateFiles(directory, "*" + query + "*", SearchOption.AllDirectories);
+
+            string[] splitQuery = SplitMe(query, symbols);
+
+            List<string> foundFilenames;
+
+            if (fileNames == null)
+            {
+               fileNames = [];
+            }
+
+            fileNames.Clear();
+
+            foreach (string substring in splitQuery)
+            {
+                foundFilenames = (List<string>)Directory.EnumerateFiles(directory, "*" + substring + "*", SearchOption.AllDirectories);
+
+                if (fileNames != null)
+                {
+                    // TODO: O(n); optimize this
+                    foreach (string fname in foundFilenames)
+                    {
+                        // Add the filename to our filename list.
+                        fileNames.Add(fname);
+                    }
+                }
+            }
         }
     }
 }
