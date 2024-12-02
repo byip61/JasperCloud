44e0dcb540e49d7bfc2ff0e3cb65adbdc3de3af6
mCreese-mancol <mcreese01@manhattan.edu>
2024-12-02 04:04:01 -0500

    Update HTML

--- JasperCloud/Controllers/HomeController.cs
+++ JasperCloud/Controllers/HomeController.cs
@@ -18,6 +18,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Main()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
